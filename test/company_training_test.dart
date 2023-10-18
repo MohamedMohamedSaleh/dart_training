b0c65d801288daefc31d2be017f05970dfbf2052
MohamedMohamedSaleh <alrisha77777@gmail.com>
2023-10-18 14:55:14 +0300

    last Edit (not important)

--- test/company_training_test.dart
+++ test/company_training_test.dart
@@ -1,8 +1,14 @@
 import 'package:company_training/company_training.dart';
-import 'package:test/test.dart';
+
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+}
+
+void expect(int calculate, int i) {
+}
+
+void test(String s, Null Function() param1) {
 }
